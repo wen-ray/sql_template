eeeb31407620dcd35c47843c6bde315657d2aa6e
wen-ray <63574233+wen-ray@users.noreply.github.com>
2023-07-06 16:47:10 +0800

    12

--- src/main.js
+++ src/main.js
@@ -33,6 +33,40 @@ function CallbackFunc(Error, Results){
     Results.forEach(data => console.log(data));
 }
 
-let sql = ``
+// let sql = `
+//     CREATE TABLE IF NOT EXISTS Users(
+//     ID TEXT PRIMARY KEY,
+//     Username TEXT,
+//     Email TEXT
+//     DOB TEXT
+//     );
+// `
+// let sql = `
+//     SELECT name FROM pragma_table_info("Users");
+// `
+// let sql = `
+//     ALTER TABLE Users
+//     RENAME COLUMN Email TO Emali;
+// `
+// let sql=`
+//     INSERT INTO Users(ID, Username, Emali, DOB)
+//     VALUES("1234567888/88","Timmy","timmy@failure.org","2005-10-28");
+// `
+// let show = `SELECT name FROM pragma_table_info("Users"); `
 
-db.exec(sql, CallbackFunc);
+let sql=`
+    DELETE FROM Users
+    WHERE id = "1234567";
+`
+
+// db.exec(sql);
+
+db.all(sql, CallbackFunc);
+// db.all(sql,(error, results)=>{
+
+//     if(error)return console.error(error);
+//     results.forEach(row =>{
+//         console.log(row);
+//     });
+// });
+
